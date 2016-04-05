9d2441776e77d8a95cfc58fd4af657cbd52cd9c9
paul shorey <ps@artspaces.net>
2016-04-04 22:50:20 -0400

    test.js mongo

---
+++ test.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cats');
+
+var Cat = mongoose.model('Cat', { name: String });
+
+var kitty = new Cat({ name: 'Zildjian' });
+kitty.save(function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('meow');
+  }
+});
