069beb53a4e6bf99d8815abd51d370670d42d910
paul shorey <ps@artspaces.net>
2016-04-05 12:43:29 -0400

    db startup

--- _boot/app.sh
+++ _boot/app.sh
@@ -2,9 +2,6 @@ iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 10
 
 cd /www/api-nyc
 
-mongod
-node test.js
-
 i=0;
 while true; do
 	i=$[$i+1]
---
+++ _boot/db.sh
@@ -0,0 +1,7 @@
+mongod
+
+cd /www/api-nyc
+node db.js
+
+cd /www/api-nyc/admin
+npm start
