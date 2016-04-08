37aaf3e2ae5a4d94d262f2acde775fe0161d78ff
paul (local) <ps@artspaces.net>
2016-04-07 22:59:33 -0400

    git pull before starting

--- _boot/app.sh
+++ _boot/app.sh
@@ -1,6 +1,10 @@
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 1080
 
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/ps1-git
 cd /www/api-nyc
+git reset HEAD -\-hard;
+git pull
 
 i=0;
 while true; do
