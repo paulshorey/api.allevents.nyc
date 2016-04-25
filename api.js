ad2d4d030ccc571dd868b6cdde8d6dffa60a9fd7
paul (local) <ps@artspaces.net>
2016-04-25 16:05:11 -0400

    cors options

--- api.js
+++ api.js
@@ -18,9 +18,17 @@ process.env.PATH = __dirname;
 process.app = process.inc.express();
 // process.app.use(process.cors());
 process.app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
 });
 process.app.use(process.inc.express_parser.json({
 	limit: '50mb'
