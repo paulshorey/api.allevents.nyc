e807e5d932a71c3d66b18bcd63280d49f37bf7b9
paul (local) <ps@artspaces.net>
2016-06-07 13:16:40 -0400

    deploy script

--- _deploy.sh
+++ _deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# eval "$(ssh-agent -s)"
-# ssh-add ~/.ssh/ps1-git
-# cd /www/go-nyc
-# git reset HEAD -\-hard;
-# git pull
-# node go.js
+
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/ps1-git
+cd /www/api-nyc
+git reset HEAD -\-hard;
+git pull
 
 killall node
 reboot
