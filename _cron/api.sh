9dd9ac805a75986f1b94c74715c8050eeed68cc5
paul (local) <ps@artspaces.net>
2016-08-02 07:46:25 -0400

    https

--- _cron/api.sh
+++ _cron/api.sh
@@ -1,10 +1,10 @@
 # attempt to renew SSL before messing with port :80
-/opt/letsencrypt/letsencrypt-auto renew
-/etc/init.d/nginx reload
+# /opt/letsencrypt/letsencrypt-auto renew
+# /etc/init.d/nginx reload
 
 
 # start app
-# iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 1080
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 1080
 
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/ps1-git
--- _cron/weekly.sh
+++ _cron/weekly.sh
@@ -2,3 +2,32 @@
 
 # /opt/letsencrypt/letsencrypt-auto renew >> /var/log/le-renew.log
 # /etc/init.d/nginx reload
+
+
+
+
+# vim /etc/nginx/sites-available/default
+
+# server {
+#   listen 80;
+#   server_name localhost;
+#   return 301 https://$host$request_uri;
+# }
+# server {
+#   listen 443 ssl;
+#   server_name localhost;
+
+#   ssl_certificate /etc/letsencrypt/live/api.allevents.nyc/fullchain.pem;
+#   ssl_certificate_key /etc/letsencrypt/live/api.allevents.nyc/privkey.pem;
+
+#   root /usr/share/nginx/html;
+#   index index.html index.htm index.nginx-debian.html;
+
+#   location / {
+#     try_files $uri $uri/ =404;
+#   }
+
+#   location ^~ /.well-known/ {
+#     allow all;
+#   }
+# }
