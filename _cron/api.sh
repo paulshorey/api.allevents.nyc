96bdd5ff011591056c536963e56821fcc6a4a588
paul (local) <ps@artspaces.net>
2016-08-02 02:01:45 -0400

    fixed ssl

--- _cron/api.sh
+++ _cron/api.sh
@@ -1,9 +1,15 @@
-# iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 1080
+# attempt to renew SSL before messing with port :80
+/opt/letsencrypt/letsencrypt-auto renew
+/etc/init.d/nginx reload
 
-# eval "$(ssh-agent -s)"
-# ssh-add ~/.ssh/ps1-git
-# cd /www/api-nyc
-# git reset HEAD -\-hard;
-# git pull
 
-# node api.js
+# start app
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 1080
+
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/ps1-git
+cd /www/api-nyc
+git reset HEAD -\-hard;
+git pull
+
+node api.js
--- _cron/weekly.sh
+++ _cron/weekly.sh
@@ -1,2 +1,4 @@
-/opt/letsencrypt/letsencrypt-auto renew >> /var/log/le-renew.log
-/etc/init.d/nginx reload
+# sudo /opt/letsencrypt/letsencrypt-auto certonly -a webroot --webroot-path=/usr/share/nginx/html -d api.allevents.nyc
+
+# /opt/letsencrypt/letsencrypt-auto renew >> /var/log/le-renew.log
+# /etc/init.d/nginx reload
